006415982aea88c1abecf629a41cd6b1ae19c4b8
Petr Devaikin <p.devaikin@gmail.com>
2015-05-29 18:17:51 +0300

    change color

--- rts/main.js
+++ rts/main.js
@@ -1,18 +1,25 @@
 var img;  // Declare variable 'img'.
+var lines = [];
 
 var WIDTH = 650,
     HEIGHT = 300,
     INIT_MAX_SPEED = 3,
 
-    POINT_COUNT,
-    LINES_COUNT,
-    ITERATION_COUNT;
+    NOISE_MAX_R = 30,
+    NOISE_COUNT = 500,
 
+    params = {};
+
+
+function grabIntParam(inputId) {
+    return parseInt(document.getElementById(inputId).value);
+}
 
 function grabParams() {
-    LINES_COUNT = document.getElementById('lineCount').value;
-    POINT_COUNT = document.getElementById('pointCount').value;
-    ITERATION_COUNT = document.getElementById('iterationCount').value;
+    params.LINES_COUNT = grabIntParam('lineCount');
+    params.POINT_COUNT = grabIntParam('pointCount');
+    params.ITERATION_COUNT = grabIntParam('iterationCount');
+    params.SPOT_BRIGHTNESS = grabIntParam('spotBrightness');
 }
 
 
@@ -21,68 +28,139 @@ function setup() {
     
     var inputs = document.querySelectorAll('#controls input');
     for (var i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener("input", clearAndDraw, false);
+        inputs[i].addEventListener("input", function() {
+            grabParams();
+            generatePath();
+            clearAndDraw();
+        });
     }
-    document.querySelector('#controls button').addEventListener("click", clearAndDraw, false);
+
+    document.querySelector('#changeShape').addEventListener("click", function() {
+        generatePath();
+        clearAndDraw();
+    });
+
+    document.querySelector('#changeColor').addEventListener("click", function() {
+        changeColor();
+        clearAndDraw();
+    });
+
+    document.querySelector('#changeAll').addEventListener("click", function() {
+        generatePath();
+        changeColor()
+        clearAndDraw();
+    });
+
+    clearAndDraw();
+
+
+    grabParams();
+    generatePath();
+    changeColor()
 
     clearAndDraw();
 }
 
-function clearAndDraw() {
-    console.log('clear and draw');
-
+function clearCanvas() {
+    smooth();
+    colorMode(RGB, 255);
     fill(10, 10, 10);
     rect(0, 0, WIDTH, HEIGHT);
-
-    grabParams();
-    drawBg();
 }
 
-function init(hue) {
-    colorMode(HSB, 255);
+function clearAndDraw() {
+    console.log('clear and draw');
+    clearCanvas();
+    drawPath();
+}
 
+function initLine() {
     var points = [];
-    for (var i = 0; i < POINT_COUNT; i++)
+    for (var i = 0; i < params.POINT_COUNT; i++)
         points.push({
-            x: random(WIDTH),
-            y: random(HEIGHT),
             angle: random(2 * Math.PI),
-            speed: random(INIT_MAX_SPEED)
+            speed: random(INIT_MAX_SPEED),
+            steps: [{
+                x: random(WIDTH),
+                y: random(HEIGHT)
+            }],
         });
     return {
         points: points,
-        color: color(hue, 100, 100, 10)
+        color: undefined
+    }
+}
+
+function changeColor() {
+    colorMode(HSB, 255);
+    for (var l = 0; l < lines.length; l++) {
+        lines[l].color = color(random(255), 100, 100, 10);
     }
 }
 
 function doStep(points) {
     for (var i = 0; i < points.length; i++) {
-        points[i].x += sin(points[i].angle) * points[i].speed;
-        points[i].y += cos(points[i].angle) * points[i].speed;
+        var lastPoint = points[i].steps[points[i].steps.length - 1];
+        points[i].steps.push({
+            x: lastPoint.x + sin(points[i].angle) * points[i].speed,
+            y: lastPoint.y + cos(points[i].angle) * points[i].speed,
+        });
     }
 }
 
-function drawPoints(points, color) {
+function generatePath() {
+    var newLines = [];
+    for (var i = 0; i < params.LINES_COUNT; i++) {
+        var newLine = initLine();
+        if (i < lines.length)
+            newLine.color = lines[i].color;
+        else
+            newLine.color = color(random(255), 100, 100, 10);
+
+        newLines.push(newLine);
+    }
+    
+    for (var l = 0; l < newLines.length; l++)
+        for (var i = 0; i < params.ITERATION_COUNT; i++) {
+            doStep(newLines[l].points);
+        }
+
+    lines = newLines;
+}
+
+function addNoise() {
+    colorMode(RGB, 255);
+
+    for (var i = 0; i < NOISE_COUNT; i++) {
+        fill(0, 0, 0, params.SPOT_BRIGHTNESS);
+        noStroke();
+        ellipse(
+            random(WIDTH),
+            random(HEIGHT),
+            randomGaussian(NOISE_MAX_R, NOISE_MAX_R / 3),
+            randomGaussian(NOISE_MAX_R, NOISE_MAX_R / 3)
+        );
+    }
+}
+
+function drawLines(points, step, color) {
     stroke(color);
     noFill();
     beginShape();
-    curveVertex(points[0].x, points[0].y);
+    curveVertex(points[0].steps[step].x, points[0].steps[step].y);
     for (var i = 0; i < points.length; i++)
-        curveVertex(points[i].x, points[i].y);
-    curveVertex(points[i - 1].x, points[i - 1].y);
+        curveVertex(points[i].steps[step].x, points[i].steps[step].y);
+    curveVertex(points[i - 1].steps[step].x, points[i - 1].steps[step].y);
     endShape();
 }
 
-function drawBg() {
-    var lines = [];
-    for (var i = 0; i < LINES_COUNT; i++)
-        lines.push(init(random(255)));
-    
-    for (var l = 0; l < lines.length; l++)
-        for (var i = 0; i < ITERATION_COUNT; i++) {
-            drawPoints(lines[l].points, lines[l].color);
-            doStep(lines[l].points);
+function drawPath() {
+    for (var i = 0; i < params.ITERATION_COUNT; i++)
+        for (var l = 0; l < lines.length; l++) {
+            drawLines(lines[l].points, i, lines[l].color);
         }
+
+    addNoise();
 }
 
 function draw() {
