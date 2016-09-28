b78abdb52da288a99140e3acfcec72e3659a1082
Petr Devaikin <p.devaikin@gmail.com>
2016-09-28 18:21:52 +0300

    transitions

--- duma/v2/data/transitions.js
+++ duma/v2/data/transitions.js
@@ -1,49 +1,46 @@
-var d_transitions = {
-    "5707": {
-        "from": 5707,
-        "to": 5725,
-        "number": 20
-    },
-    "5708": { "from": 5708, "to": 5724, "number": 29 },
-    "5709": { "from": 5709, "to": 5723, "number": 20 },
-    "5710": { "from": 5710, "to": 5725, "number": 6 },
-    "5711": { "from": 5711, "to": 5725, "number": 10 },
-    "5712": { "from": 5712, "to": 5719, "number": 12 },
-    "5713": { "from": 5713, "to": 5723, "number": 24 },
-    "5714": { "from": 5714, "to": 5724, "number": 35 },
-    "5715": { "from": 5715, "to": 5725, "number": 4 },
-    "5716": { "from": 5716, "to": 5719, "number": 4 },
-    "5717": { "from": 5717, "to": 5725, "number": 8 },
-    "5718": { "from": 5718, "to": 5724, "number": 4 },
-    "5719": { "from": 5719, "to": 5735, "number": 20 },
-    "5720": { "from": 5720, "to": 5731, "number": 11 },
-    "5721": { "from": 5721, "to": 5728, "number": 64 },
-    "5722": { "from": 5722, "to": 5730, "number": 27 },
-    "5723": { "from": 5723, "to": 5735, "number": 19 },
-    "5724": { "from": 5724, "to": 5735, "number": 21 },
-    "5725": { "from": 5725, "to": 5735, "number": 13 },
-    "5726": { "from": 5726, "to": 5730, "number": 13 },
-    "5727": { "from": 5727, "to": 5740, "number": 40 },
-    "5728": { "from": 5728, "to": 5738, "number": 11 },
-    "5729": { "from": 5729, "to": 5738, "number": 28 },
-    "5730": { "from": 5730, "to": 5738, "number": 47 },
-    "5731": { "from": 5731, "to": 5733, "number": 51 },
-    "5732": { "from": 5732, "to": 5738, "number": 27 },
-    "5733": { "from": 5733, "to": 5738, "number": 16 },
-    "5734": { "from": 5734, "to": 5738, "number": 3 },
-    "5735": { "from": 5735, "to": 5738, "number": 4 },
-    "5736": { "from": 5736, "to": 5738, "number": 9 },
-    "5737": { "from": 5737, "to": 5743, "number": 19 },
-    "5738": { "from": 5738, "to": 5744, "number": 172 },
-    "5739": { "from": 5739, "to": 5745, "number": 9 },
-    "5740": { "from": 5740, "to": 5745, "number": 5 },
-    "5741": { "from": 5741, "to": 5746, "number": 32 },
-    "5743": { "from": 5743, "to": 5748, "number": 22 },
-    "5744": { "from": 5744, "to": 5748, "number": 108 },
-    "5745": { "from": 5745, "to": 5748, "number": 31 },
-    "5746": { "from": 5746, "to": 5747, "number": 46 }
+var d_transitions = {"5707":{"5721":{"from":5707,"to":5721,"number":1},"5724":{"from":5707,"to":5724,"number":1},"5725":{"from":5707,"to":5725,"number":1},"5726":{"from":5707,"to":5726,"number":14},"5730":{"from":5707,"to":5730,"number":1},"5733":{"from":5707,"to":5733,"number":1},"5744":{"from":5707,"to":5744,"number":1}},"5708":{"5720":{"from":5708,"to":5720,"number":21},"5724":{"from":5708,"to":5724,"number":8}},"5709":{"5723":{"from":5709,"to":5723,"number":18},"5724":{"from":5709,"to":5724,"number":2}},"5710":{"5719":{"from":5710,"to":5719,"number":2},"5725":{"from":5710,"to":5725,"number":2},"5729":{"from":5710,"to":5729,"number":1},"5738":{"from":5710,"to":5738,"number":1}},"5711":{"5719":{"from":5711,"to":5719,"number":5},"5722":{"from":5711,"to":5722,"number":1},"5724":{"from":5711,"to":5724,"number":1},"5725":{"from":5711,"to":5725,"number":2},"5731":{"from":5711,"to":5731,"number":1}},"5712":{"5719":{"from":5712,"to":5719,"number":1},"5724":{"from":5712,"to":5724,"number":7},"5735":{"from":5712,"to":5735,"number":4}},"5713":{"5719":{"from":5713,"to":5719,"number":7},"5722":{"from":5713,"to":5722,"number":6},"5723":{"from":5713,"to":5723,"number":1},"5724":{"from":5713,"to":5724,"number":7},"5725":{"from":5713,"to":5725,"number":2},"5748":{"from":5713,"to":5748,"number":1}},"5714":{"5721":{"from":5714,"to":5721,"number":29},"5724":{"from":5714,"to":5724,"number":3},"5726":{"from":5714,"to":5726,"number":3}},"5715":{"5719":{"from":5715,"to":5719,"number":1},"5724":{"from":5715,"to":5724,"number":1},"5725":{"from":5715,"to":5725,"number":2}},"5716":{"5719":{"from":5716,"to":5719,"number":2},"5722":{"from":5716,"to":5722,"number":1},"5726":{"from":5716,"to":5726,"number":1}},"5717":{"5719":{"from":5717,"to":5719,"number":2},"5722":{"from":5717,"to":5722,"number":1},"5724":{"from":5717,"to":5724,"number":1},"5725":{"from":5717,"to":5725,"number":2},"5731":{"from":5717,"to":5731,"number":1},"5732":{"from":5717,"to":5732,"number":1}},"5718":{"5722":{"from":5718,"to":5722,"number":1},"5724":{"from":5718,"to":5724,"number":1},"5727":{"from":5718,"to":5727,"number":1},"5738":{"from":5718,"to":5738,"number":1}},"5719":{"5729":{"from":5719,"to":5729,"number":2},"5730":{"from":5719,"to":5730,"number":2},"5731":{"from":5719,"to":5731,"number":1},"5732":{"from":5719,"to":5732,"number":12},"5735":{"from":5719,"to":5735,"number":1},"5738":{"from":5719,"to":5738,"number":1},"5748":{"from":5719,"to":5748,"number":1}},"5720":{"5728":{"from":5720,"to":5728,"number":1},"5729":{"from":5720,"to":5729,"number":1},"5731":{"from":5720,"to":5731,"number":1},"5736":{"from":5720,"to":5736,"number":6},"5737":{"from":5720,"to":5737,"number":1},"5749":{"from":5720,"to":5749,"number":1}},"5721":{"5727":{"from":5721,"to":5727,"number":54},"5728":{"from":5721,"to":5728,"number":2},"5729":{"from":5721,"to":5729,"number":1},"5733":{"from":5721,"to":5733,"number":5},"5739":{"from":5721,"to":5739,"number":1},"5741":{"from":5721,"to":5741,"number":1}},"5722":{"5727":{"from":5722,"to":5727,"number":4},"5728":{"from":5722,"to":5728,"number":1},"5729":{"from":5722,"to":5729,"number":1},"5730":{"from":5722,"to":5730,"number":1},"5732":{"from":5722,"to":5732,"number":7},"5733":{"from":5722,"to":5733,"number":7},"5739":{"from":5722,"to":5739,"number":1},"5740":{"from":5722,"to":5740,"number":1},"5741":{"from":5722,"to":5741,"number":1},"5744":{"from":5722,"to":5744,"number":2},"5746":{"from":5722,"to":5746,"number":1}},"5723":{"5728":{"from":5723,"to":5728,"number":2},"5730":{"from":5723,"to":5730,"number":1},"5734":{"from":5723,"to":5734,"number":14},"5735":{"from":5723,"to":5735,"number":2}},"5724":{"5728":{"from":5724,"to":5728,"number":5},"5729":{"from":5724,"to":5729,"number":5},"5730":{"from":5724,"to":5730,"number":1},"5732":{"from":5724,"to":5732,"number":1},"5734":{"from":5724,"to":5734,"number":1},"5735":{"from":5724,"to":5735,"number":4},"5738":{"from":5724,"to":5738,"number":3},"5743":{"from":5724,"to":5743,"number":1}},"5725":{"5728":{"from":5725,"to":5728,"number":1},"5729":{"from":5725,"to":5729,"number":2},"5730":{"from":5725,"to":5730,"number":2},"5731":{"from":5725,"to":5731,"number":5},"5735":{"from":5725,"to":5735,"number":1},"5738":{"from":5725,"to":5738,"number":2}},"5726":{"5730":{"from":5726,"to":5730,"number":3},"5731":{"from":5726,"to":5731,"number":2},"5733":{"from":5726,"to":5733,"number":7},"5741":{"from":5726,"to":5741,"number":1}},"5727":{"5739":{"from":5727,"to":5739,"number":3},"5740":{"from":5727,"to":5740,"number":1},"5741":{"from":5727,"to":5741,"number":25},"5746":{"from":5727,"to":5746,"number":7},"5747":{"from":5727,"to":5747,"number":4}},"5728":{"5738":{"from":5728,"to":5738,"number":1},"5739":{"from":5728,"to":5739,"number":3},"5740":{"from":5728,"to":5740,"number":5},"5745":{"from":5728,"to":5745,"number":2}},"5729":{"5738":{"from":5729,"to":5738,"number":21},"5739":{"from":5729,"to":5739,"number":4},"5740":{"from":5729,"to":5740,"number":1},"5744":{"from":5729,"to":5744,"number":2}},"5730":{"5738":{"from":5730,"to":5738,"number":47}},"5731":{"5733":{"from":5731,"to":5733,"number":1},"5738":{"from":5731,"to":5738,"number":49},"5748":{"from":5731,"to":5748,"number":1}},"5732":{"5738":{"from":5732,"to":5738,"number":18},"5739":{"from":5732,"to":5739,"number":5},"5740":{"from":5732,"to":5740,"number":2},"5744":{"from":5732,"to":5744,"number":1},"5745":{"from":5732,"to":5745,"number":1}},"5733":{"5738":{"from":5733,"to":5738,"number":4},"5739":{"from":5733,"to":5739,"number":3},"5740":{"from":5733,"to":5740,"number":1},"5741":{"from":5733,"to":5741,"number":7},"5746":{"from":5733,"to":5746,"number":1}},"5734":{"5738":{"from":5734,"to":5738,"number":1},"5740":{"from":5734,"to":5740,"number":2}},"5735":{"5738":{"from":5735,"to":5738,"number":3},"5745":{"from":5735,"to":5745,"number":1}},"5736":{"5737":{"from":5736,"to":5737,"number":6},"5738":{"from":5736,"to":5738,"number":1},"5739":{"from":5736,"to":5739,"number":1},"5740":{"from":5736,"to":5740,"number":1}},"5737":{"5743":{"from":5737,"to":5743,"number":18},"5749":{"from":5737,"to":5749,"number":1}},"5738":{"5744":{"from":5738,"to":5744,"number":168},"5745":{"from":5738,"to":5745,"number":1},"5748":{"from":5738,"to":5748,"number":2},"5750":{"from":5738,"to":5750,"number":1}},"5739":{"5745":{"from":5739,"to":5745,"number":8},"5748":{"from":5739,"to":5748,"number":1}},"5740":{"5745":{"from":5740,"to":5745,"number":4},"5750":{"from":5740,"to":5750,"number":1}},"5741":{"5746":{"from":5741,"to":5746,"number":31},"5747":{"from":5741,"to":5747,"number":1}},"5743":{"5748":{"from":5743,"to":5748,"number":1},"5749":{"from":5743,"to":5749,"number":20},"5750":{"from":5743,"to":5750,"number":1}},"5744":{"5748":{"from":5744,"to":5748,"number":108}},"5745":{"5748":{"from":5745,"to":5748,"number":5},"5750":{"from":5745,"to":5750,"number":26}},"5746":{"5747":{"from":5746,"to":5747,"number":46}}}
+
+var temp = {}
+
+var a_transitions_direct = []
+var a_transitions_jump = []
+
+for (var i in d_transitions) {
+    var a = []
+    for (var j in d_transitions[i]) {
+        a.push(d_transitions[i][j]);
+
+        if (temp[j] === undefined)
+            temp[j] = []
+        temp[j].push(d_transitions[i][j]);
+    }
+
+    a.sort(function(a, b) {
+        return d_fractions[a.to].offset - d_fractions[b.to].offset;
+    });
+
+    var offset = 0;
+    for (var i in a) {
+        a[i].leftOffset = offset;
+        offset += a[i].number;
+    }
 }
 
+for (var i in temp) {
+    temp[i].sort(function(a, b) {
+        return d_fractions[a.from].offset - d_fractions[b.from].offset;
+    })
 
+    var offset = 0;
+    for (var j in temp[i]) {
+        temp[i][j].rightOffset = offset;
+        offset += temp[i][j].number;
+    }
+}
 
-var a_transitions = Object.keys(d_transitions).map(a => d_transitions[a]);
+for (var i in temp)
+    for (var j in temp[i])
+        if (d_fractions[temp[i][j].from].convocationId == d_fractions[temp[i][j].to].convocationId - 1)
+            a_transitions_direct.push(temp[i][j]);
+        else
+            a_transitions_jump.push(temp[i][j]);
--- duma/v2/main.js
+++ duma/v2/main.js
@@ -14,7 +14,7 @@ if (!String.prototype.format) {
 
 
 var CONVO_MARGIN = 100,
-    CONVO_OFFSET = 100,
+    CONVO_OFFSET = 200,
 
     WIDTH = 1000,
     HEIGHT = 200,
@@ -24,7 +24,12 @@ var CONVO_MARGIN = 100,
     FRACTION_LEVEL_Y = 5;
 
 
-var svg;
+var dom = {
+    svg: undefined,
+    convocations: undefined,
+    fractions: undefined,
+    transitions: undefined,
+}
 
 //=====================
 
@@ -38,11 +43,11 @@ function fractionPosition(fraction) {
 
 
 function drawConvocations() {
-    var convocations = svg.selectAll('.convocation').data(a_convocations).enter();
+    var convocations = dom.convocations.selectAll('.convocation').data(a_convocations).enter();
 
     var groups = convocations.append('g')
         .classed('convocation', true)
-        .attr('transform', d => 'translate({0},0)'.format(CONVO_MARGIN + (d.number - 1) * CONVO_OFFSET));
+        .attr('transform', function(d) { return 'translate({0},0)'.format(CONVO_MARGIN + (d.number - 1) * CONVO_OFFSET); });
     groups.append('line')
         .attr('x1', 0)
         .attr('x2', 0)
@@ -51,63 +56,100 @@ function drawConvocations() {
 }
 
 function drawTransitions() {
-    var transitions = svg.selectAll('.transition').data(a_transitions).enter();
+    var transitionsDirect = dom.transitionsDirect.selectAll('.transition').data(a_transitions_direct).enter();
+    var transitionsJump = dom.transitionsJump.selectAll('.transition').data(a_transitions_jump).enter();
 
-    var groups = transition.append('g')
-        .classed('transition', true);
+    function drawHelper(transitions) {
+        var groups = transitions.append('g')
+            .classed('transition', true);
 
-    groups.append('path')
-        .attr('d', function(d) {
-            return 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}';
-        });
+        groups.append('path')
+            .attr('d', function(d) {
+                var f1 = d_fractions[d.from],
+                    f2 = d_fractions[d.to];
+                var result = 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
+                    f1._position[0] + FRACTION_WIDTH / 2 + FRACTION_LEVEL_X,
+                    f1._position[1] + FRACTION_LEVEL_Y + d.leftOffset,
+                    f2._position[0] - FRACTION_WIDTH / 2 - FRACTION_LEVEL_X,
+                    f2._position[1] + FRACTION_LEVEL_Y + d.rightOffset,
+                    f2._position[0] - FRACTION_WIDTH / 2 - FRACTION_LEVEL_X,
+                    f2._position[1] + FRACTION_LEVEL_Y + d.number + d.rightOffset,
+                    f1._position[0] + FRACTION_WIDTH / 2 + FRACTION_LEVEL_X,
+                    f1._position[1] + FRACTION_LEVEL_Y + d.number + d.leftOffset
+                );
+
+                return result;
+            })
+            .attr('fill', function(d) {
+                return d_fractions[d.from]._color;
+            });
+    }
+
+    drawHelper(transitionsDirect);
+    drawHelper(transitionsJump);
 }
 
 function drawFractions() {
-    var fractions = svg.selectAll('.fraction').data(a_fractions).enter();
+    var fractions = dom.fractions.selectAll('.fraction').data(a_fractions).enter();
 
     var groups = fractions.append('g')
         .classed('fraction', true)
-        .attr('transform', d => String.prototype.format.apply('translate({0},{1})', fractionPosition(d)));
+        .attr('transform', function(d) {
+            d._position = fractionPosition(d);
+            d._color = d_parties[d.partyId].color;
+            return 'translate({0},{1})'.format(d._position[0], d._position[1]);
+        });
 
     groups.append('rect')
         .attr('x', -FRACTION_WIDTH / 2)
         .attr('y', 0)
         .attr('width', FRACTION_WIDTH)
-        .attr('height', d => d.size)
-        .attr('fill', d => d_parties[d.partyId].color);
+        .attr('height', function(d) { return d.size; })
+        .attr('fill', function(d) { return d._color; });
 
     groups.append('path')
-        .attr('d', d => 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
-            FRACTION_WIDTH / 2, 0,
-            FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
-            FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-            FRACTION_WIDTH / 2, d.size
-        ))
-        .attr('fill', d => d3.color(d_parties[d.partyId].color).darker(.5));
+        .attr('d', function(d) {
+            return 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
+                FRACTION_WIDTH / 2, 0,
+                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
+                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
+                FRACTION_WIDTH / 2, d.size
+            );
+        })
+        .attr('fill', function(d) { return d3.color(d._color).darker(.5); });
 
     groups.append('path')
-        .attr('d', d => 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
-            -FRACTION_WIDTH / 2, 0,
-            -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
-            -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-            -FRACTION_WIDTH / 2, d.size
-        ))
-        .attr('fill', d => d3.color(d_parties[d.partyId].color).darker(.5));
+        .attr('d', function(d) {
+            return 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
+                -FRACTION_WIDTH / 2, 0,
+                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
+                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
+                -FRACTION_WIDTH / 2, d.size
+            );
+        })
+        .attr('fill', function(d) { return d3.color(d._color).darker(.5); });
 
-    groups.filter(d => d.partyId == 8).append('path')
-        .attr('d', d => 'M{0},{1}L{2},{3}L{4},{5},L{6},{7}z'.format(
-            FRACTION_WIDTH / 2, d.size,
-            FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-            -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-            -FRACTION_WIDTH / 2, d.size
-        ))
-        .attr('fill', d => d3.color(d_parties[d.partyId].color).darker(1));
+    groups.filter(function(d) { return d.partyId == 8; }).append('path')
+        .attr('d', function(d) {
+            return 'M{0},{1}L{2},{3}L{4},{5},L{6},{7}z'.format(
+                FRACTION_WIDTH / 2, d.size,
+                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
+                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
+                -FRACTION_WIDTH / 2, d.size
+            );
+        })
+        .attr('fill', function(d) { return d3.color(d_parties[d.partyId].color).darker(1); });
 }
 
 
 function draw() {
-    svg = d3.select('svg');
+    dom.svg = d3.select('svg');
+    dom.convocations = dom.svg.select('.convocations');
+    dom.fractions = dom.svg.select('.fractions');
+    dom.transitionsDirect = dom.svg.select('.transitions .direct');
+    dom.transitionsJump = dom.svg.select('.transitions .jump');
 
     drawConvocations();
     drawFractions();
+    drawTransitions();
 }
