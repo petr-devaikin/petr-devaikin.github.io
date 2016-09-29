e2b4c19bc5bc11fb1a698a4448f052be8d7a2907
Petr Devaikin <p.devaikin@gmail.com>
2016-09-29 17:11:26 +0300

    gradients

--- duma/v2/main.js
+++ duma/v2/main.js
@@ -14,14 +14,14 @@ if (!String.prototype.format) {
 
 
 var CONVO_MARGIN = 100,
-    CONVO_OFFSET = 200,
+    CONVO_OFFSET = 150,
 
     WIDTH = 1000,
     HEIGHT = 200,
 
-    FRACTION_WIDTH = 20,
-    FRACTION_LEVEL_X = 5,
-    FRACTION_LEVEL_Y = 5;
+    FRACTION_WIDTH = 10,
+
+    SHADOW_K = .1;
 
 
 var dom = {
@@ -29,6 +29,7 @@ var dom = {
     convocations: undefined,
     fractions: undefined,
     transitions: undefined,
+    defs: undefined,
 }
 
 //=====================
@@ -37,7 +38,7 @@ var dom = {
 function fractionPosition(fraction) {
     return [
         (CONVO_MARGIN + (d_convocations[fraction.convocationId].number - 1) * CONVO_OFFSET),
-        fraction.offset
+        fraction.offset + fraction.order * 3
     ]
 }
 
@@ -59,30 +60,60 @@ function drawTransitions() {
     var transitionsDirect = dom.transitionsDirect.selectAll('.transition').data(a_transitions_direct).enter();
     var transitionsJump = dom.transitionsJump.selectAll('.transition').data(a_transitions_jump).enter();
 
+    var grads = dom.defs.selectAll('linearGradient')
+        .data(a_transitions_direct.concat(a_transitions_jump), function(d) { return d.from + '-' + d.to })
+        .enter()
+            .append('linearGradient')
+            .attr('id', function(d) { return 'g' + d.from + '-' + d.to; })
+            .attr('x1', 0)
+            .attr('y1', 0)
+            .attr('x2', 1)
+            .attr('y2', 0);
+
+    grads.append('stop')
+        .attr('offset', '0%')
+        .attr('stop-color', function(d) { return d_fractions[d.from]._color; });
+
+    grads.append('stop')
+        .attr('offset', '100%')
+        .attr('stop-color', function(d) { return d_fractions[d.to]._color; });
+
     function drawHelper(transitions) {
         var groups = transitions.append('g')
             .classed('transition', true);
 
-        groups.append('path')
+        var lines1 = groups.filter(function(d) {
+            return d_fractions[d.from]._position[1] + d.leftOffset == d_fractions[d.to]._position[1] + d.rightOffset;
+        })
+
+        lines1.append('rect')
+            .attr('x', function(d) { return d_fractions[d.from]._position[0] + FRACTION_WIDTH / 2; })
+            .attr('y', function(d) { return d_fractions[d.from]._position[1] + d.leftOffset; })
+            .attr('width', function(d) { return d_fractions[d.to]._position[0] - d_fractions[d.from]._position[0] - FRACTION_WIDTH; })
+            .attr('height', function(d) { return d.number; })
+            .attr('fill', function(d) { return 'url(#g{0}-{1})'.format(d.from, d.to); })
+            .attr('stroke', 'none');
+
+        var lines2 = groups.filter(function(d) {
+            return d_fractions[d.from]._position[1] + d.leftOffset != d_fractions[d.to]._position[1] + d.rightOffset;
+        })
+
+        lines2.append('path')
             .attr('d', function(d) {
                 var f1 = d_fractions[d.from],
                     f2 = d_fractions[d.to];
-                var result = 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
-                    f1._position[0] + FRACTION_WIDTH / 2 + FRACTION_LEVEL_X,
-                    f1._position[1] + FRACTION_LEVEL_Y + d.leftOffset,
-                    f2._position[0] - FRACTION_WIDTH / 2 - FRACTION_LEVEL_X,
-                    f2._position[1] + FRACTION_LEVEL_Y + d.rightOffset,
-                    f2._position[0] - FRACTION_WIDTH / 2 - FRACTION_LEVEL_X,
-                    f2._position[1] + FRACTION_LEVEL_Y + d.number + d.rightOffset,
-                    f1._position[0] + FRACTION_WIDTH / 2 + FRACTION_LEVEL_X,
-                    f1._position[1] + FRACTION_LEVEL_Y + d.number + d.leftOffset
+                var result = 'M {0} {1} C {2} {3}, {4} {5}, {6} {7}'.format(
+                    f1._position[0] + FRACTION_WIDTH / 2, f1._position[1] + d.leftOffset + d.number / 2,
+                    (f1._position[0] + f2._position[0]) / 2, f1._position[1] + d.leftOffset + d.number / 2,
+                    (f1._position[0] + f2._position[0]) / 2, f2._position[1] + d.rightOffset + d.number / 2,
+                    f2._position[0] - FRACTION_WIDTH / 2, f2._position[1] + d.rightOffset + d.number / 2
                 );
 
                 return result;
             })
-            .attr('fill', function(d) {
-                return d_fractions[d.from]._color;
-            });
+            .attr('fill', 'none')
+            .attr('stroke', function(d) { return 'url(#g{0}-{1})'.format(d.from, d.to); })
+            .attr('stroke-width', function(d) { return d.number; });
     }
 
     drawHelper(transitionsDirect);
@@ -106,39 +137,6 @@ function drawFractions() {
         .attr('width', FRACTION_WIDTH)
         .attr('height', function(d) { return d.size; })
         .attr('fill', function(d) { return d._color; });
-
-    groups.append('path')
-        .attr('d', function(d) {
-            return 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
-                FRACTION_WIDTH / 2, 0,
-                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
-                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-                FRACTION_WIDTH / 2, d.size
-            );
-        })
-        .attr('fill', function(d) { return d3.color(d._color).darker(.5); });
-
-    groups.append('path')
-        .attr('d', function(d) {
-            return 'M{0},{1}L{2},{3}L{4},{5}L{6},{7}z'.format(
-                -FRACTION_WIDTH / 2, 0,
-                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, FRACTION_LEVEL_Y,
-                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-                -FRACTION_WIDTH / 2, d.size
-            );
-        })
-        .attr('fill', function(d) { return d3.color(d._color).darker(.5); });
-
-    groups.filter(function(d) { return d.partyId == 8; }).append('path')
-        .attr('d', function(d) {
-            return 'M{0},{1}L{2},{3}L{4},{5},L{6},{7}z'.format(
-                FRACTION_WIDTH / 2, d.size,
-                FRACTION_WIDTH / 2 + FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-                -FRACTION_WIDTH / 2 - FRACTION_LEVEL_X, d.size + FRACTION_LEVEL_Y,
-                -FRACTION_WIDTH / 2, d.size
-            );
-        })
-        .attr('fill', function(d) { return d3.color(d_parties[d.partyId].color).darker(1); });
 }
 
 
@@ -148,6 +146,7 @@ function draw() {
     dom.fractions = dom.svg.select('.fractions');
     dom.transitionsDirect = dom.svg.select('.transitions .direct');
     dom.transitionsJump = dom.svg.select('.transitions .jump');
+    dom.defs = dom.svg.select('defs');
 
     drawConvocations();
     drawFractions();
