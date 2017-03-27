d41051d52c1c938bed7c6510241120a299bd8297
Petr Devaikin <pd@Petrs-MacBook-Pro.local>
2017-03-27 13:49:15 +0200

    bubblechart

--- nesta/js/helpers.js
+++ nesta/js/helpers.js
@@ -9,3 +9,32 @@ if (!String.prototype.format) {
 		});
 	};
 }
+
+if (!Number.prototype.abbrNum) {
+	Number.prototype.abbrNum = function(decPlaces) {
+		var number = this + 0;
+
+		if (decPlaces === undefined) decPlaces = 0;
+		decPlaces = Math.pow(10,decPlaces);
+
+		var abbrev = [ "k", "m", "b", "t" ];
+
+		for (var i=abbrev.length-1; i>=0; i--) {
+			var size = Math.pow(10,(i+1)*3);
+
+			if (size <= number) {
+				number = Math.round(number*decPlaces/size)/decPlaces;
+
+				if ((number == 1000) && (i < abbrev.length - 1)) {
+					number = 1;
+					i++;
+				}
+
+				number += abbrev[i];
+				break;
+			}
+		}
+
+		return number;
+	}
+}
