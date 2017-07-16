42a4443b5d66a86e8e3f7392f29e4c7f6eea23fc
Petr Devaikin <p.devaikin@gmail.com>
2017-07-16 21:05:30 +0200

    year

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function() {
-	console.log('12');
-})
+	setYear();
+});
+
+function setYear() {
+	var currentYear;
+	$(".project").each(function() {
+		if ($(this).attr('data-year') == currentYear) {
+			$(".project__year", this).hide();
+		}
+		else {
+			currentYear = $(this).attr('data-year');
+			$(".project__year", this).show();
+		}
+	});
+}
