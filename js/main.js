ba748bda7205b1fbb37e39219046b3f80ebbb676
Petr Devaikin <p.devaikin@gmail.com>
2017-07-28 17:25:56 +0200

    cross

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function() {
-	setYear();
+	hasher.changed.add(handleChanges); //add hash change listener
+	hasher.initialized.add(handleChanges); //add initialized listener (to grab initial value in case it is already set)
+	hasher.init();
+
+	$('.menu__main a').append('<div class="cross"></div>');
+	$('.cross').click(function() {
+		hasher.setHash('');
+		return false;
+	});
 });
 
 function setYear() {
@@ -13,4 +21,28 @@ function setYear() {
 			$(".project__year", this).show();
 		}
 	});
+}
+
+function selectSection(sectionName) {
+	$(".menu__main a").each(function() {
+		if ($(this).attr("href") == "#" + sectionName) {
+			$(this).addClass("selected");
+		}
+		else {
+			$(this).removeClass("selected");
+		}
+	});
+
+	$(".project").each(function() {
+		if (sectionName == '' || $(this).attr('data-type').split(',').indexOf(sectionName) != -1)
+			$(this).show();
+		else
+			$(this).hide();
+	});
+
+	setYear();
+}
+
+function handleChanges(newHash, oldHash) {
+	selectSection(newHash);
 }
